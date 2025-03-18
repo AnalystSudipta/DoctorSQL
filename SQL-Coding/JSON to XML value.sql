dbb41292330a2ad26eb8c674d73d8b47b7563ed1
Sudipta Biswas <143230091+AnalystSudipta@users.noreply.github.com>
2025-03-18 16:26:21 +0600

    Create JSON to XML value.sql

---
+++ SQL-Coding/JSON to XML value.sql
@@ -0,0 +1,56 @@
+-- Table Creation:
+
+CREATE TABLE nested_json_tab (
+    id NUMBER PRIMARY KEY,
+    json_data CLOB CONSTRAINT nested_json_data_is_json CHECK (json_data IS JSON)
+);
+
+
+
+-- Insert Data:
+
+INSERT INTO nested_json_tab (id, json_data) VALUES (1, '{
+    "name": "John",
+    "age": 30,
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "country": "USA"
+    },
+    "contacts": [
+        {"type": "email", "value": "john@example.com"},
+        {"type": "phone", "value": "123-456-7890"}
+    ]
+}');
+
+
+-- SQL:
+
+SELECT
+
+ XMLELEMENT("JSON_To_XML",
+            XMLAGG(XMLELEMENT("JSON_To_XML_TAG",
+                              XMLELEMENT("Name", JT.NAME),
+                              XMLELEMENT("Age", JT.AGE),
+                              XMLELEMENT("Street", JT.street),
+                              XMLELEMENT("City", JT.city),
+                              XMLELEMENT("Country", JT.country),
+                              XMLELEMENT("Contact_type", JT.contact_type),
+                              XMLELEMENT("Contact_value", JT.contact_value)
+                              
+                              ))) XML_TAG
+
+  FROM nested_json_tab,
+       json_table(json_data,
+                  '$[*]'
+                  COLUMNS(NAME VARCHAR2(50) PATH '$.name',
+                          age NUMBER PATH '$.age',
+                          NESTED PATH '$.address[*]'
+                          COLUMNS(street VARCHAR2(50) PATH '$.street',
+                                  city VARCHAR2(50) PATH '$.city',
+                                  country VARCHAR2(50) PATH '$.country'),
+                          NESTED PATH '$.contacts[*]'
+                          COLUMNS(contact_type VARCHAR2(50) PATH '$.type',
+                                  contact_value VARCHAR2(50) PATH '$.value')
+                          
+                          )) jt;
